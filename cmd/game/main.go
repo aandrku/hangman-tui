34fea936ce419e061a4bf6043cac83142196ccfb
Andrii Sozonik <sozonik04.av@gmail.com>
2025-04-24 17:40:56 -0700

    add cmd/game/main.go - an entry file for the game

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"hangman-tui/pkg/core"
+	"hangman-tui/pkg/scenes/menus/home"
+	"hangman-tui/pkg/screen"
+	"hangman-tui/pkg/words"
+)
 
 func main() {
-	fmt.Println("Hello, Hangman TUI")
+	// load custom words if specified by user
+	wordsFile := flag.String("words", "", "usage: hangman -words<path/to/the/file/with/words>")
+	flag.Parse()
+	if *wordsFile != "" {
+		s := words.GetStore()
+		s.ReadFromFile(*wordsFile)
+	}
+
+	// initialize screen package
+	screen.Init()
+
+	// start the core with home menu
+	c := core.GetInstance()
+	c.SetScene(home.NewMenu())
+	c.Run()
+
 }
